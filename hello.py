8d13c2a23bf3a87552281882667c7e0b556b55e6
Amrendra Singh <amrsingh29@users.noreply.github.com>
2021-04-12 15:42:00 +0800

    MCSM-3730

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@
 # Another test.
 print("hello world")
 print("This is Amrendra")
-print("From BMC Software Singapore")
+print("Fdrom BMC Software Singapore")
 print("Barossa Garden")
 print("ABC")
 print("MCSM-37dfd30BfMC HeMCSM-puMdsCSM-373d0sher.head_coMCSM-3730mmit.message3730lixy79MCSM-3730MCSM-37301d")
